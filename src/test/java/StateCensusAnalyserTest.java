39f613568adb297fb9c4fa0d34c368b578c4fd08
prithvi0 <prithviy96@gmail.com>
2020-04-07 20:50:05 +0530

    Conflict issue pending

--- src/test/java/StateCensusAnalyserTest.java
+++ src/test/java/StateCensusAnalyserTest.java
@@ -8,6 +8,7 @@ public class StateCensusAnalyserTest {
     private static final String INDIAN_CENSUS_CSV_FILE_PATH = "./src/test/resources/StateCensusData.csv";
     private static final String INDIAN_CENSUS_CSV_WRONG_FILE = "./src/test/resources/StateCensusDate.csv";
     private static final String INDIAN_CENSUS_CSV_WRONG_FILE_TYPE = "./src/test/resources/StateCensusDate.aac";
+    private static final String INDIAN_CENSUS_CSV_WRONG_FILE_DELIMITER = "./src/test/resources/StateCensusDataDelimiter.csv";
 
     StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
 
@@ -40,6 +41,16 @@ public class StateCensusAnalyserTest {
             stateCensusAnalyser.CensusCSVData(INDIAN_CENSUS_CSV_WRONG_FILE_TYPE);
         } catch (StateCensusAnalyserException e) {
             Assert.assertEquals(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE, e.type);
+            e.printStackTrace();
+        }
+    }
+    // T.C 1.4: TEST CASE TO RETURN A CUSTOM EXCEPTION IF THE FILE DELIMITER IS INCORRECT
+    @Test
+    public void givenStateCensusFile_WhenIncorrectDelimiter_ReturnCustomException() throws IOException, NullPointerException {
+        try {
+            stateCensusAnalyser.CensusCSVData(INDIAN_CENSUS_CSV_WRONG_FILE_DELIMITER);
+        } catch (StateCensusAnalyserException e) {
+            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE_TYPE, e.type);
             e.printStackTrace();
         }
     }
