ad6e115ec2c09a1d66eb560da4f9e0c7c7210275
prithvi0 <prithviy96@gmail.com>
2020-04-07 05:52:19 +0530

    Added testcase to pass a custom exception for wrong file type

--- src/test/java/StateCensusAnalyserTest.java
+++ src/test/java/StateCensusAnalyserTest.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 public class StateCensusAnalyserTest {
     private static final String INDIAN_CENSUS_CSV_FILE_PATH = "./src/test/resources/StateCensusData.csv";
     private static final String INDIAN_CENSUS_CSV_WRONG_FILE = "./src/test/resources/StateCensusDate.csv";
+    private static final String INDIAN_CENSUS_CSV_WRONG_FILE_TYPE = "./src/test/resources/StateCensusDate.aac";
 
     StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
 
@@ -26,6 +27,16 @@ public class StateCensusAnalyserTest {
     public void givenStateCensusCSVFile_IfIncorrect_ReturnCustomException() throws IOException, NullPointerException {
         try {
             stateCensusAnalyser.CensusCSVData(INDIAN_CENSUS_CSV_WRONG_FILE);
+        } catch (StateCensusAnalyserException e) {
+            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE, e.type);
+            e.printStackTrace();
+        }
+    }
+    // T.C 1.3: TEST CASE TO RETURN A CUSTOM EXCEPTION IF FILE TYPE IS INCORRECT
+    @Test
+    public void givenStateCensusFile_IfIncorrectFileType_ReturnCustomException() throws IOException, NullPointerException {
+        try {
+            stateCensusAnalyser.CensusCSVData(INDIAN_CENSUS_CSV_WRONG_FILE_TYPE);
         } catch (StateCensusAnalyserException e) {
             Assert.assertEquals(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE, e.type);
             e.printStackTrace();
